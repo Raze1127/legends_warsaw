3705067cad9c52ed808419f30f5ea0aa71a12354
Raze <73419971+Raze1127@users.noreply.github.com>
2023-07-20 18:10:07 +0300

    handle schedule in start

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ def phone_number_handler(update: Update, context: CallbackContext):
     send_main_menu(update, context)
 
 
+
 # Отправка главного меню
 def send_main_menu(update: Update, context: CallbackContext) -> None:
     language = context.user_data.get('language', 'russian')
@@ -153,6 +154,7 @@ def question_text_handler(update: Update, context: CallbackContext):
                              text=text)
     context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=f'Вопрос от {update.effective_user.first_name}: {question}')
     send_main_menu(update, context)
+    handle_schedule(update, context)
 
 
 def handle_profile(update: Update, context: CallbackContext):
